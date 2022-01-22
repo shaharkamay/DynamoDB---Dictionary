942f4f9bc5b48d4ece9f9384409b5caa8690b8cd
shaharKamay <shahar.kamay@gmail.com>
2022-01-22 11:05:03 +0200

    serverless test2

--- server/src/utils/config/index.ts
+++ server/src/utils/config/index.ts
@@ -5,8 +5,8 @@ const config = {
   aws: {
     region: 'eu-central-1',
     endpoint: 'http://dynamodb.eu-central-1.amazonaws.com',
-    accessKeyId: process.env.ACCESS_KEY_ID,
-    secretAccessKey: process.env.SECRET_ACCESS_KEY,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   },
   port: process.env.PORT || 8080,
 };
